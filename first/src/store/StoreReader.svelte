b219f0aecfecbf9a1795471fe349a30de256a7b9
Hansmu <hans.miikael@gmail.com>
2024-06-12 09:12:28 +0300

    Add subscription clearing to avoid memory leaks

--- first/src/store/StoreReader.svelte
+++ first/src/store/StoreReader.svelte
@@ -1,11 +1,19 @@
 <script lang="ts">
     import { writableStoreExample } from './store';
+    import {onDestroy} from "svelte";
 
     let storeNumber: number = 0;
 
-    writableStoreExample.subscribe(
+    // Each time you set up a subscription, you need to also unsubscribe
+    // If this element gets unmounted, then the subscription will still live on
+    // The subscribe method will return an unsubscribe method
+    const unsubscribe = writableStoreExample.subscribe(
         val => (storeNumber = val)
-    )
+    );
+
+    onDestroy(() => {
+        unsubscribe();
+    });
 </script>
 
 <div>
