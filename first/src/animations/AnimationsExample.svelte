f577fbf0db80010ead946fb5c1b33b93858e1ce1
Hansmu <hans.miikael@gmail.com>
2024-06-14 08:52:57 +0300

    Add spring store example

--- first/src/animations/AnimationsExample.svelte
+++ first/src/animations/AnimationsExample.svelte
@@ -6,7 +6,19 @@
     // For different easings, you can import them from this Svelte package
     import { cubicIn } from 'svelte/easing';
 
+    // Spring is another animation library, but this one takes physics into account and has different kinds of configuration options
+    import { spring } from "svelte/motion";
+
     const progress = tweened(0);
+
+    const springStore = spring({
+        rotation: 10,
+        dx: 0
+    }, {
+        stiffness: 0.05,
+        damping: 0.9,
+        precision: 0.001
+    });
 
     // If you react to the store, you'll see that the store value changes multiple times, all the way until it has reached its designated value.
     // $: console.log($progress);
@@ -22,8 +34,45 @@
             // interpolate
         });
     }, 1500);
+
+    const toggleCardIntoView = () => {
+        if ($springStore.dx) {
+            springStore.set({
+                dx: 0,
+                rotation: 10,
+            });
+        } else {
+            springStore.set({
+                dx: 1200,
+                rotation: 120,
+            });
+        }
+    };
+
+    // When logging out the spring store values, the same thing can be seen, it's simply a reactive store, that is constantly changing its values
+    // From the starting value to the final value
+    // $: console.log($springStore);
 </script>
+
+<style>
+    .card {
+        position: absolute;
+        width: 20rem;
+        height: 30rem;
+        background: #ccc;
+        left: calc(50% - 10rem);
+        top: calc(50vh - 15rem);
+        box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26);
+        border-radius: 5px;
+    }
+</style>
 
 <h2>Motion, Transitions & Animations</h2>
 
 <progress value={$progress}/>
+
+<button on:click={toggleCardIntoView}>
+    Toggle card
+</button>
+
+<div class="card" style="transform: rotateZ({$springStore.rotation}deg) translateX({$springStore.dx}px)" />
