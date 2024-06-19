0a912e1ea8754dbea217bed874a41b65db0a3d66
Hansmu <hans.miikael@gmail.com>
2024-06-19 09:20:58 +0300

    Add recursive component example

--- first/src/specialElements/SpecialElements.svelte
+++ first/src/specialElements/SpecialElements.svelte
@@ -1,1 +1,26 @@
+<script lang="ts">
+    import TreeNode, {type Node} from "./TreeNode.svelte";
+
+    const familyStructure: Node[] = [
+        {
+            isParent: true,
+            name: "Chris",
+            children: [
+                {
+                    isParent: true,
+                    name: "Moe",
+                    children: [{isParent: false, name: "Julie"}]
+                }
+            ]
+        },
+        {isParent: false, name: "Anna"}
+    ];
+</script>
+
 <h2>Special elements example</h2>
+
+<h3>Recursive component example</h3>
+
+{#each familyStructure as familyMember}
+    <TreeNode member={familyMember} />
+{/each}
---
+++ first/src/specialElements/TreeNode.svelte
@@ -0,0 +1,28 @@
+<script context="module" lang="ts">
+    export type Node = {
+        name: string;
+        isParent: boolean;
+        children?: Node[];
+    };
+</script>
+
+<script lang="ts">
+    export let member: Node;
+</script>
+
+<style>
+    div {
+        margin-left: 1rem;
+    }
+</style>
+
+<div>
+    <p>{member.name}</p>
+    {#if member.isParent}
+        {#each (member.children || []) as child}
+            <!-- svelte:self can be used to do recursive rendering -->
+            <!-- to render another instance of the component itself from itself, you can use svelte:self -->
+            <svelte:self member={child} />
+        {/each}
+    {/if}
+</div>
